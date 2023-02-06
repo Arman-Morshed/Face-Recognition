839355a9d568db2c401b1498bb1935f7b2677f09
Istiak Morsalin <64763332+istiakmorsalinmlbd@users.noreply.github.com>
2023-02-06 15:49:18 +0600

    Revert "Deepface apple silicon implementation"

--- deepface/deepface/deepfacetest.py
+++
@@ -1,4 +0,0 @@
-from deepface import DeepFace
-
-result = DeepFace.verify(img1_path = "/Users/istiakmorsalin/iOffice/deepface-master/tests/dataset/img1.jpg", img2_path = "/Users/istiakmorsalin/iOffice/deepface-master/tests/dataset/img2.jpg")
-print(result)
